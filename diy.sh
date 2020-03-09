7397ba46ce7d9fac935fc96df60ad85ccc3d6200
lzxsx600 <lzxsx600@gmail.com>
2020-03-09 23:31:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/0.20.120.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.20.120.71/g' package/base-files/files/bin/config_generate
